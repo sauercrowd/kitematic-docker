2f2c8544b4bef53f2ab14f5b3293d397f85e8d15
Jonas Otten <jonadev95@users.noreply.github.com>
2015-08-07 14:43:48 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -f; IFS=$'\n'
-MIT-MAGIC-COOKIES=($(xauth list))
+mitcookies=($(xauth list))
 set +f; unset IFS
-sudo docker run -i --net host --name kitematic -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY  -e MIT-MAGIC-COOKIES=$MIT-MAGIC-COOKIES -v /var/run/docker.sock:/var/run/docker.sock --privileged=true -t jonadev95/kitematic-docker
+sudo docker run -i --net host --name kitematic -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY  -e MAGICCOOKIES=$mitcookies -v /var/run/docker.sock:/var/run/docker.sock --privileged=true -t jonadev95/kitematic-docker
